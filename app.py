06f6fe42e3a83e103154fcfeb67c93c6c494742a
Bryan A. Guapacha F <bryanicesi0822@gmail.com>
2023-07-07 01:22:21 -0500

    Read and load data into the new DB
    We are able to read and load the data into the new database and we can visualize the data with PgAdmin or DBeaver.

--- app.py
+++ app.py
@@ -1,32 +1,56 @@
 from flask import Flask, jsonify
 import csv
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="guapi_db",
+    user="bryan",
+    password="password"
+)
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'Hello, World! --> This is the technical exercise to join Globant as a Data Engineer Jr.'
 
-def read_csv(file_path):
+# def read_csv(file_path):
+#     with open(file_path, 'r') as file:
+#         reader = csv.reader(file)
+#         data = list(reader)
+#     return data
+
+def read_csv(file_path, table_name):
     with open(file_path, 'r') as file:
         reader = csv.reader(file)
-        data = list(reader)
-    return data
+        cursor = conn.cursor()
+        try:
+            for row in reader:
+                insert_query = f"INSERT INTO {table_name} VALUES ({','.join(['%s']*len(row))})"
+                cursor.execute(insert_query, row)
+            conn.commit()
+        except Exception as e:
+            conn.rollback()  # Revertir cualquier cambio realizado en la transacción
+            print("Error during data insertion:", e)
+        finally:
+            cursor.close()
+
 
 @app.route('/departments', methods=['GET'])
-def obtener_departamentos():
-    departamentos = read_csv('data/departments.csv')
-    return jsonify(departamentos)
+def get_departments():
+    departments = read_csv('data/departments.csv', 'departments')
+    return jsonify(departments)
 
 @app.route('/jobs', methods=['GET'])
-def obtener_trabajos():
-    trabajos = read_csv('data/jobs.csv')
-    return jsonify(trabajos)
+def get_jobs():
+    jobs = read_csv('data/jobs.csv', 'jobs')
+    return jsonify(jobs)
 
 @app.route('/employees', methods=['GET'])
-def obtener_empleados():
-    empleados = read_csv('data/hired_employees.csv')
-    return jsonify(empleados)
+def get_employees():
+    employees = read_csv('data/hired_employees.csv', 'employees')
+    return jsonify(employees)
 
 if __name__ == '__main__': 
     app.run(debug=True)
