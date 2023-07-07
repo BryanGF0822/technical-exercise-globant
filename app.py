827f29263683654e4c4802e567897ebcbd832815
Bryan A. Guapacha F <bryanicesi0822@gmail.com>
2023-07-07 03:03:51 -0500

    Batch data insertions (1 to 1000) are achieved
    Batch data insertions (1 to 1000) are achieved with the help of the execute_batch method provided by the psycopg2.extras module.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
 import csv
 import psycopg2
+import psycopg2.extras
+from psycopg2.extras import execute_batch
 
 conn = psycopg2.connect(
     host="localhost",
@@ -15,52 +17,93 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World! --> This is the technical exercise to join Globant as a Data Engineer Jr.'
 
-# def read_csv(file_path):
-#     with open(file_path, 'r') as file:
-#         reader = csv.reader(file)
-#         data = list(reader)
-#     return data
-
 def read_csv(file_path, table_name):
     with open(file_path, 'r') as file:
         reader = csv.reader(file)
-        cursor = conn.cursor()
-        try:
-            for row in reader:
-                insert_query = f"INSERT INTO {table_name} VALUES ({','.join(['%s']*len(row))})"
-                cursor.execute(insert_query, row)
-            conn.commit()
-        except Exception as e:
-            conn.rollback()  # Revertir cualquier cambio realizado en la transacción
-            print("Error during data insertion:", e)
-        finally:
-            cursor.close()
-
-    # Después de cargar los datos, se debe realizar una nueva consulta para obtener los datos desde la base de datos
-    cursor = conn.cursor()
-    select_query = f"SELECT * FROM {table_name}"
-    cursor.execute(select_query)
-    data = cursor.fetchall()
-    cursor.close()
-
-    # Devuelve los datos cargados desde la base de datos
+        data = list(reader)
     return data
 
 
-@app.route('/departments', methods=['GET'])
-def get_departments():
-    departments = read_csv('data/departments.csv', 'departments')
-    return jsonify(departments)
-
 @app.route('/jobs', methods=['GET'])
 def get_jobs():
-    jobs = read_csv('data/jobs.csv', 'jobs')
-    return jsonify(jobs)
+    data = read_csv('data/jobs.csv', 'jobs')
+    cursor = conn.cursor()
+    try:
+        insert_query = f"INSERT INTO jobs VALUES (%s, %s)"
+        page_size = 1000
+        num_rows = len(data)
+        rows_inserted = 0
 
+        while rows_inserted < num_rows:
+            batch = data[rows_inserted:rows_inserted + page_size]
+            execute_batch(cursor, insert_query, batch)
+            rows_inserted += len(batch)
+            print(f"Filas insertadas: {rows_inserted}/{num_rows}")
+
+        conn.commit()
+
+        print(f"Se insertaron {rows_inserted} filas en lotes de 1 a {page_size}")
+
+        return jsonify({"message": "Datos insertados correctamente"})
+    except Exception as e:
+        conn.rollback()
+        return jsonify({"error": f"Error durante la inserción de datos: {str(e)}"}), 500
+    finally:
+        cursor.close()
+
+@app.route('/departments', methods=['GET'])
+def get_departments():
+    data = read_csv('data/departments.csv', 'departments')
+    cursor = conn.cursor()
+    try:
+        insert_query = f"INSERT INTO departments VALUES (%s, %s)"
+        page_size = 1000
+        num_rows = len(data)
+        rows_inserted = 0
+
+        while rows_inserted < num_rows:
+            batch = data[rows_inserted:rows_inserted + page_size]
+            execute_batch(cursor, insert_query, batch)
+            rows_inserted += len(batch)
+            print(f"Filas insertadas: {rows_inserted}/{num_rows}")
+
+        conn.commit()
+
+        print(f"Se insertaron {rows_inserted} filas en lotes de 1 a {page_size}")
+
+        return jsonify({"message": "Datos insertados correctamente"})
+    except Exception as e:
+        conn.rollback()
+        return jsonify({"error": f"Error durante la inserción de datos: {str(e)}"}), 500
+    finally:
+        cursor.close()
+        
 @app.route('/employees', methods=['GET'])
 def get_employees():
-    employees = read_csv('data/hired_employees.csv', 'employees')
-    return jsonify(employees)
+    data = read_csv('data/hired_employees.csv', 'employees')
+    cursor = conn.cursor()
+    try:
+        insert_query = f"INSERT INTO employees VALUES (%s, %s, %s, %s, %s)"
+        page_size = 1000
+        num_rows = len(data)
+        rows_inserted = 0
+
+        while rows_inserted < num_rows:
+            batch = data[rows_inserted:rows_inserted + page_size]
+            execute_batch(cursor, insert_query, batch)
+            rows_inserted += len(batch)
+            print(f"Filas insertadas: {rows_inserted}/{num_rows}")
+
+        conn.commit()
+
+        print(f"Se insertaron {rows_inserted} filas en lotes de 1 a {page_size}")
+
+        return jsonify({"message": "Datos insertados correctamente"})
+    except Exception as e:
+        conn.rollback()
+        return jsonify({"error": f"Error durante la inserción de datos: {str(e)}"}), 500
+    finally:
+        cursor.close()
 
 if __name__ == '__main__': 
     app.run(debug=True)
