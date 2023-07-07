56c4719ad7107408c45aaf7ed2b9f46dcea28faa
Bryan A. Guapacha F <bryanicesi0822@gmail.com>
2023-07-06 22:30:57 -0500

    Flask is installed and the methods for receiving data from CSV files are performed.
    Flask is installed and the methods for receiving data from CSV files, which can be displayed in JSON format, are implemented.

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
 import csv
-import psycopg2
 
 app = Flask(__name__)
 
-#@app.route('/')
-#def hello():
-#    return 'Hello, World!'
+@app.route('/')
+def hello():
+    return 'Hello, World!'
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    # Leer archivos CSV y cargar los datos en la base de datos
-    # ...
-    return 'Data uploaded successfully'
+def read_csv(file_path):
+    with open(file_path, 'r') as file:
+        reader = csv.reader(file)
+        data = list(reader)
+    return data
 
-@app.route('/batch_insert', methods=['POST'])
-def batch_insert():
-    # Procesar la solicitud de inserción por lotes
-    # ...
-    return 'Batch insert completed'
+@app.route('/departments', methods=['GET'])
+def obtener_departamentos():
+    departamentos = read_csv('data/departments.csv')
+    return jsonify(departamentos)
 
+@app.route('/jobs', methods=['GET'])
+def obtener_trabajos():
+    trabajos = read_csv('data/jobs.csv')
+    return jsonify(trabajos)
 
-if __name__ == '__main__':
+@app.route('/employees', methods=['GET'])
+def obtener_empleados():
+    empleados = read_csv('data/hired_employees.csv')
+    return jsonify(empleados)
+
+if __name__ == '__main__': 
     app.run(debug=True)
