ba7793d28fa345ed8b6c7065d0a3772a3f9a1744
Bryan A. Guapacha F <bryanicesi0822@gmail.com>
2023-07-08 03:00:10 -0500

    End-points are added for SQL section 2.

--- app.py
+++ app.py
@@ -107,6 +107,109 @@ def get_employees():
     finally:
         cursor.close()
 
+
+# @app.route('/employees/2021/quarters', methods=['GET'])
+# def get_employees_by_quarter():
+#     cursor = conn.cursor()
+#     try:
+#         query = """
+#         SELECT d.department_name, j.job_title, EXTRACT(QUARTER FROM e.hire_date) AS quarter, COUNT(*) AS num_employees
+#         FROM employees e
+#         INNER JOIN departments d ON e.department_id = d.department_id
+#         INNER JOIN jobs j ON e.job_id = j.job_id
+#         WHERE EXTRACT(YEAR FROM e.hire_date) = 2021
+#         GROUP BY d.department_name, j.job_title, quarter
+#         ORDER BY d.department_name, j.job_title
+#         """
+#         cursor.execute(query)
+#         result = cursor.fetchall()
+
+#         data = []
+#         for row in result:
+#             department_name, job_title, quarter, num_employees = row
+#             data.append({
+#                 "department_name": department_name,
+#                 "job_title": job_title,
+#                 "quarter": int(quarter),
+#                 "num_employees": int(num_employees)
+#             })
+
+#         return jsonify(data)
+#     except Exception as e:
+#         return jsonify({"error": f"Error al obtener los datos: {str(e)}"}), 500
+#     finally:
+#         cursor.close()
+
+@app.route('/employees/2021/quarters', methods=['GET'])
+def get_employees_by_quarter():
+    cursor = conn.cursor()
+    try:
+        query = """
+        SELECT d.department_name, j.job_title,
+               COUNT(CASE WHEN EXTRACT(QUARTER FROM e.hire_date) = 1 THEN 1 ELSE NULL END) AS q1,
+               COUNT(CASE WHEN EXTRACT(QUARTER FROM e.hire_date) = 2 THEN 1 ELSE NULL END) AS q2,
+               COUNT(CASE WHEN EXTRACT(QUARTER FROM e.hire_date) = 3 THEN 1 ELSE NULL END) AS q3,
+               COUNT(CASE WHEN EXTRACT(QUARTER FROM e.hire_date) = 4 THEN 1 ELSE NULL END) AS q4
+        FROM employees e
+        INNER JOIN departments d ON e.department_id = d.department_id
+        INNER JOIN jobs j ON e.job_id = j.job_id
+        WHERE EXTRACT(YEAR FROM e.hire_date) = 2021
+        GROUP BY d.department_name, j.job_title
+        ORDER BY d.department_name, j.job_title
+        """
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        data = []
+        for row in result:
+            department_name, job_title, q1, q2, q3, q4 = row
+            data.append({
+                "department_name": department_name,
+                "job_title": job_title,
+                "Q1": int(q1),
+                "Q2": int(q2),
+                "Q3": int(q3),
+                "Q4": int(q4)
+            })
+
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": f"Error al obtener los datos: {str(e)}"}), 500
+    finally:
+        cursor.close()
+
+
+@app.route('/departments/more_employees_than_mean', methods=['GET'])
+def get_departments_with_more_employees_than_mean():
+    cursor = conn.cursor()
+    try:
+        query = """
+        SELECT d.department_id, d.department_name, COUNT(*) AS num_employees
+        FROM departments d
+        INNER JOIN employees e ON e.department_id = d.department_id
+        WHERE EXTRACT(YEAR FROM e.hire_date) = 2021
+        GROUP BY d.department_id, d.department_name
+        HAVING COUNT(*) > (SELECT AVG(num_employees) FROM (SELECT COUNT(*) AS num_employees FROM employees WHERE EXTRACT(YEAR FROM hire_date) = 2021 GROUP BY department_id) AS subquery)
+        ORDER BY num_employees DESC
+        """
+        cursor.execute(query)
+        result = cursor.fetchall()
+
+        data = []
+        for row in result:
+            department_id, department_name, num_employees = row
+            data.append({
+                "department_id": int(department_id),
+                "department_name": department_name,
+                "num_employees": int(num_employees)
+            })
+
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": f"Error al obtener los datos: {str(e)}"}), 500
+    finally:
+        cursor.close()
+
 if __name__ == '__main__':
     app.config.from_object(config['development'])
 
